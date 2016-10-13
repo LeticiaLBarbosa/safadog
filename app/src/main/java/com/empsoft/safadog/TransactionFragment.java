b8e897395df5153fd1205418dc2e2bd43c45fa2c
arthurlustosa <arthur.souza@ccc.ufcg.edu.br>
2016-10-13 14:56:15 -0300

    tela de transações iniciada

--- app/src/main/java/com/empsoft/safadog/TransactionFragment.java
+++ app/src/main/java/com/empsoft/safadog/TransactionFragment.java
@@ -11,6 +11,6 @@ public class TransactionFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_fragment, container, false);
+        return inflater.inflate(R.layout.transaction_fragment, container, false);
     }
 }
