dcf10260e5f7c737da8fec2562c4f7f157ed903d
arthurlustosa <arthur.souza@ccc.ufcg.edu.br>
2016-10-13 14:55:34 -0300

    tela de matching iniciada

--- app/src/main/java/com/empsoft/safadog/MatchingFragments.java
+++ app/src/main/java/com/empsoft/safadog/MatchingFragments.java
@@ -11,6 +11,6 @@ public class MatchingFragments extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_3, container, false);
+        return inflater.inflate(R.layout.matching_fragments, container, false);
     }
 }
