791fca76dfed652139f726d71ed02102cf268c6b
arthurlustosa <arthur.souza@ccc.ufcg.edu.br>
2016-10-13 15:22:52 -0300

    tela de filtros iniciada

--- app/src/main/java/com/empsoft/safadog/FiltersFragment.java
+++ app/src/main/java/com/empsoft/safadog/FiltersFragment.java
@@ -11,6 +11,6 @@ public class FiltersFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.filtersFragment, container, false);
+        return inflater.inflate(R.layout.filters_fragment, container, false);
     }
 }
