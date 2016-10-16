e0a9ceb667cd2cc6cf89128c5726e420cc2f7498
Leticia Barbosa <leticiabarbosa94@gmail.com>
2016-10-16 18:00:16 -0300

    Configurando a action bar e adicionando a tela do feed

--- app/src/main/java/com/empsoft/safadog/MainActivity.java
+++ app/src/main/java/com/empsoft/safadog/MainActivity.java
@@ -3,7 +3,13 @@ package com.empsoft.safadog;
 import android.app.ProgressDialog;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.internal.view.menu.ActionMenuItemView;
+import android.support.v7.widget.ActionMenuView;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,6 +22,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * MainActivity that is performed right after the login
@@ -23,16 +31,119 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    public ProgressDialog pd;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        insertInitialFragment();
 
+        // Use Display metrics to get Screen Dimensions
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
 
+        toolbar = (Toolbar) findViewById(R.id.inc_toolbar);
+        setupToolbarContent(toolbar);
 
+        toolbar.inflateMenu(R.menu.menu_actions);
+
+        // Add 10 spacing on either side of the toolbar
+        toolbar.setContentInsetsAbsolute(10, 10);
+
+        // Get the ChildCount of your Toolbar, this should only be 1
+        int childCount = toolbar.getChildCount();
+        // Get the Screen Width in pixels
+        int screenWidth = metrics.widthPixels;
+
+        // Create the Toolbar Params based on the screenWidth
+        Toolbar.LayoutParams toolbarParams = new Toolbar.LayoutParams(screenWidth, Toolbar.LayoutParams.WRAP_CONTENT);
+
+        // Loop through the child Items
+        for(int i = 0; i < childCount; i++){
+            // Get the item at the current index
+            View childView = toolbar.getChildAt(i);
+            // If its a ViewGroup
+            if(childView instanceof ViewGroup){
+                // Set its layout params
+                childView.setLayoutParams(toolbarParams);
+                // Get the child count of this view group, and compute the item widths based on this count & screen size
+                int innerChildCount = ((ViewGroup) childView).getChildCount();
+                int itemWidth  = (screenWidth / innerChildCount);
+                // Create layout params for the ActionMenuView
+                ActionMenuView.LayoutParams params = new ActionMenuView.LayoutParams(itemWidth, Toolbar.LayoutParams.WRAP_CONTENT);
+                // Loop through the children
+                for(int j = 0; j < innerChildCount; j++){
+                    View grandChild = ((ViewGroup) childView).getChildAt(j);
+                    if(grandChild instanceof ActionMenuItemView){
+                        // set the layout parameters on each View
+                        grandChild.setLayoutParams(params);
+                    }
+                }
+            }
+        }
+    }
+
+    private void insertInitialFragment() {
+        // Insert the fragment by replacing any existing fragment
+        Fragment fragment = null;
+        Class fragmentClass = FeedFragment.class;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+    }
+
+    private void setupToolbarContent(Toolbar toolbar){
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                selectItem(item);
+                return true;
+            }
+        });
+    }
+
+    private void selectItem(MenuItem menuItem){
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.action_feed:
+                fragmentClass = FeedFragment.class;
+                break;
+            case R.id.action_match:
+                fragmentClass = MatchingFragments.class;
+                break;
+            case R.id.action_filter:
+                fragmentClass = FiltersFragment.class;
+                break;
+            case R.id.action_profile:
+                fragmentClass = ProfileFragment.class;
+                break;
+            default:
+                fragmentClass = FeedFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
     }
 
     /* Called whenever we call invalidateOptionsMenu() */
